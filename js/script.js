60331be79d1337062207e5b2ac26382c86f5876b
Ítalo Oliveira <wowbrasiloficial@gmail.com>
2018-05-22 14:05:34 -0300

    front-end

---
+++ js/script.js
@@ -0,0 +1,47 @@
+$( function() {
+  $( "#tabs" ).tabs();
+} );
+
+$("#content-1").hide();
+$("#carregar_mais1").click(function(){
+  $("#content-1").show();
+  $("#carregar_mais1").hide();
+});
+
+$("#content-2").hide();
+$("#carregar_mais2").click(function(){
+  $("#content-2").show();
+  $("#carregar_mais2").hide();
+});
+
+$("#content-3").hide();
+$("#carregar_mais3").click(function(){
+  $("#content-3").show();
+  $("#carregar_mais3").hide();
+});
+
+$("#content-4").hide();
+$("#carregar_mais4").click(function(){
+  $("#content-4").show();
+  $("#carregar_mais4").hide();
+});
+
+$("#content-5").hide();
+$("#carregar_mais5").click(function(){
+  $("#content-5").show();
+  $("#carregar_mais5").hide();
+});
+
+$("#content-6").hide();
+$("#carregar_mais6").click(function(){
+  $("#content-6").show();
+  $("#carregar_mais6").hide();
+});
+
+$("#content-7").hide();
+$("#carregar_mais7").click(function(){
+  $("#content-7").show();
+  $("#carregar_mais7").hide();
+});
+
+// $("#content-2").hide();
